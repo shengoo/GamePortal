051284230c6eaa69d3b605aeac878a89311c7cf1
Qing Sheng <shengoo@hotmail.com>
2014-09-14 18:11:29 +0800

    Added controller

---
+++ GamePortal/GP.Web/Controllers/ApiTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GP.Web.Controllers
+{
+    public class ApiTestController : Controller
+    {
+        //
+        // GET: /ApiTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
